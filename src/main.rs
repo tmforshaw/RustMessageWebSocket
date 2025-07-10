2ad52aa00f14c029c5db0cef1dd5568bdea3b529
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-11 00:42:58 +0100

    Show Sender ID in message, Shutdown Tokio on Ctrl+C

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,25 @@ extern crate rocket;
 use rocket::fs::{FileServer, relative};
 use tokio::task;
 
-use crate::websocket::start_websocket;
+use crate::websocket::{start_websocket, start_websocket_listen_shutdown};
 
 mod websocket;
 
+// Launch webpage using rocket socket, mounting the static folder as the root of the webpage
 async fn launch_rocket() {
     rocket::build()
-        // .mount("/", routes![index])
         .mount("/", FileServer::from(relative!("static")))
         .launch()
         .await
         .unwrap();
 }
 
-// TODO add mpsc so i can allow communication between clients
-
 #[tokio::main]
 async fn main() {
+    // Spawn rocket and the websocket
     let rocket_handle = task::spawn(launch_rocket());
+    let websocket_handle = task::spawn(start_websocket_listen_shutdown());
 
-    let websocket_handle = task::spawn(start_websocket());
-
+    // Wait on both handles finishing
     let _ = tokio::join!(rocket_handle, websocket_handle);
 }
--- src/websocket.rs
+++ src/websocket.rs
@@ -1,8 +1,12 @@
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock, Mutex};
 
 use futures_util::{SinkExt, StreamExt};
-use tokio::net::{TcpListener, TcpStream};
-use tokio_tungstenite::accept_async;
+use tokio::{
+    net::{TcpListener, TcpStream},
+    signal,
+    sync::broadcast,
+};
+use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
 
 const WEBSOCKET_IP: &str = "127.0.0.1";
 const WEBSOCKET_PORT: &str = "4321";
@@ -10,31 +14,109 @@ const WEBSOCKET_PORT: &str = "4321";
 static WEBSOCKET_IP_AND_PORT: LazyLock<String> =
     LazyLock::new(|| format!("{WEBSOCKET_IP}:{WEBSOCKET_PORT}"));
 
-pub async fn start_websocket() {
+type Tx = tokio::sync::mpsc::UnboundedSender<Message>;
+
+pub async fn start_websocket_listen_shutdown() {
+    // Create a channel to notify websocket when to shutdown
+    let (shutdown_tx, _) = broadcast::channel(1);
+
+    // Start the websocket, providing the shutdown receiver
+    let shutdown_socket = shutdown_tx.subscribe();
+    tokio::spawn(start_websocket(shutdown_socket));
+
+    // Wait for the Ctrl+C signal to shutdown
+    signal::ctrl_c().await.unwrap();
+    println!("Shutting websocket down");
+
+    // Notify websocket to shutdown
+    let _ = shutdown_tx.send(());
+
+    // Give tasks time to shutdown
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+}
+
+pub async fn start_websocket(mut shutdown: broadcast::Receiver<()>) {
+    // Begin websocket on specified IP and Port
     let listener = TcpListener::bind(WEBSOCKET_IP_AND_PORT.clone())
         .await
         .unwrap();
 
     println!("Web socket listening on ws://{}", *WEBSOCKET_IP_AND_PORT);
 
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_connection(stream));
+    // Keep track of ongoing connections
+    let clients = Arc::new(Mutex::new(vec![]));
+
+    loop {
+        tokio::select! {
+            // Listen for new connections, then handle that connection
+            Ok((stream, _)) = listener.accept() => {
+                tokio::spawn(handle_connection(stream, clients.clone()));
+            },
+            // If shutdown receives a message, stop the socket from running
+            _ = shutdown.recv() => {
+                println!("Websocket server shutting down");
+                break;
+            }
+        }
     }
 }
 
-async fn handle_connection(stream: TcpStream) {
-    let websocket_stream = accept_async(stream).await.unwrap();
-    let (mut write, mut read) = websocket_stream.split();
+async fn handle_connection(stream: TcpStream, clients: Arc<Mutex<Vec<Tx>>>) {
+    // Get handles for the websocket, sending and receiving
+    let ws_stream = accept_async(stream).await.unwrap();
+    let (mut ws_write, mut ws_read) = ws_stream.split();
 
-    println!("New web socket connection");
+    // Create channels for communication to this client
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<Message>();
 
-    while let Some(Ok(message)) = read.next().await {
+    // Add the client's sender to clients Vec
+    let client_index = {
+        let mut clients_guard = clients.lock().unwrap();
+
+        // Get the index of this client
+        let client_index = clients_guard.len();
+        println!("New web socket connection [{client_index}]");
+
+        // Add client's mpsc sender to client list
+        clients_guard.push(tx.clone());
+
+        client_index
+    };
+
+    // Task used to send messages to the client
+    let send_task = tokio::spawn(async move {
+        while let Some(message) = rx.recv().await {
+            if ws_write.send(message).await.is_err() {
+                break;
+            }
+        }
+    });
+
+    // Receive messages from websocket and broadcast to all clients using mpsc
+    while let Some(Ok(message)) = ws_read.next().await {
         if message.is_text() || message.is_binary() {
-            println!("Recieved Message: {message:?}");
+            println!("Recieved Message [{client_index}]: {message:?}");
 
-            write.send(message).await.unwrap();
+            let clients_guard = clients.lock().unwrap();
+            for (i, client) in clients_guard.iter().enumerate() {
+                // Don't broadcast the message to your own client
+                if i != client_index {
+                    let m = Message::Text(format!("[{client_index}] {message}").into());
+                    let _ = client.send(m);
+                }
+            }
         }
     }
 
-    println!("Web socket connction closed");
+    println!("Web socket connection [{client_index}] closed");
+
+    // Stop sending to client since connection has ended
+    send_task.abort();
+
+    // Remove the tx from the clients list
+    {
+        let mut client_guard = clients.lock().unwrap();
+
+        client_guard.remove(client_index);
+    }
 }
